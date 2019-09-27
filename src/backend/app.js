4d5e961b815e31969f838dc2f9f3f11840442e0f
Aryuko <pachimaryu@gmail.com>
2019-09-27 12:04:45 +0200

    Generalize api responses

--- src/backend/app.js
+++ src/backend/app.js
@@ -18,82 +18,38 @@ const db = new Database()
 const publicPath = path.join(__dirname, '../../public')
 
 app.use(express.static(publicPath))
-app.get('/api/poll/create/:name?', (req, res) => {
-    if(req.params.name) {
-        db.createPoll(req.params.name, (data) => {
-            if (data) {
-                res.send({
-                    success: true,
-                    errormessage: false,
-                    data: data
-                })
-            } else {
-                res.send({
-                    success: false,
-                    errormessage: 'error',
-                    data: {}
-                })
-            }
-        })
-    } else {
-        res.send({
-            success: false,
-            errormessage: 'Missing name parameter',
-            data: false
-        })
-    }
+app.get('/api/poll/create/:name', (req, res) => {
+    db.createPoll(req.params.name, (data) => {
+        dbResponse(data, res)
+    })
 })
-app.get('/api/poll/:id?', (req, res) => {
-    if(req.params.id) {
-        // Get from database
-        db.getPoll(req.params.id, (data) => {
-            if (data) {
-                res.send({
-                    success: true,
-                    errormessage: false,
-                    data: data
-                })
-            } else {
-                res.send({
-                    success: false,
-                    errormessage: 'invalid poll id',
-                    data: {}
-                })
-            }
-        })
-    } else {
-        res.send({
-            success: false,
-            errormessage: 'Missing ID parameter',
-            data: false
-        })
-    }
+app.get('/api/poll/:id', (req, res) => {
+    db.getPoll(req.params.id, (data) => {
+        dbResponse(data, res)
+    })
 
 })
-app.get('/api/poll/vote/:poll_id?/:value?', (req, res) => {
-    if(req.params.poll_id && req.params.value) {
-        db.addVote(req.params.poll_id, req.params.value, (data) => {
-            if (data) {
-                res.send({
-                    success: true,
-                    errormessage: false,
-                    data: data
-                })
-            } else {
-                res.send({
-                    success: false,
-                    errormessage: 'error',
-                    data: {}
-                })
-            }
+app.get('/api/poll/vote/:poll_id/:value', (req, res) => {
+    db.addVote(req.params.poll_id, req.params.value, (data) => {
+        dbResponse(data, res)
+    })
+})
+app.get('api/*', (req, res) => { res.send('Invalid endpoint') })
+app.get('*', (req, res) => res.sendFile(path.join(publicPath, '/index.html')))
+http.listen(port, () => console.log('listening on port', port))
+
+function dbResponse(data, res) {
+    if (data) {
+        res.send({
+            success: true,
+            errormessage: false,
+            data: data
         })
     } else {
         res.send({
             success: false,
-            errormessage: 'Missing parameters',
-            data: false
+            errormessage: 'error reading from database',
+            data: {}
         })
     }
-})
-app.get('*', (req, res) => res.sendFile(path.join(publicPath, '/index.html')))
-http.listen(port, () => console.log('listening on port', port))
+}
